7498218581e0401d224e1f21fb45a8602279528c
Jonathan <jonathan.ochoa15@ct.unanleon.edu.ni>
2018-11-05 13:37:25 -0600

    Corrigiendo max_2_sum

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -26,26 +26,11 @@ def max_2_sum arr
     segundo = arr.pop
     total = primero + segundo
     return total
-  end
+end
 end
 
 def sum_to_n? arr, n
-  if !arr.any?
-    return false
-  elsif arr.length == 1
-    return false
-  else
-    i = 0
-    while i < arr.length - 2
-      j = i + 1;
-      while j < arr.length - 1
-        total = arr[i] + arr[j]
-        if total == n
-          return true
-        end
-      end
-    end
-  end
+
 end
 
 # Part 2
