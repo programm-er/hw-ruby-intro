0206d2a288993154707a7fbd3f1c39e2027531f1
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-11-05 15:24:44 -0600

    corrigiendo sum_to_n

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -7,12 +7,12 @@ def sum arr
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	# YOUR CODE HERE
 end
 
 def sum_to_n? arry, suma
    if arry.empty?
-   	suma == 0
+   	return false
   else
     arry.permutation(2).any? { |elemento| elemento[0] + elemento[1] == suma }
   end
