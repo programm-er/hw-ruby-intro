b4a02d729aae4c8208d68b06490d28e51bc4bf3e
Daybeling Lugo Blanco <daybeling.lugo15@est.unanleon.edu.ni>
2018-11-07 08:43:47 -0600

    resturando problema

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,11 +22,13 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  cadena="Hello, #{name}"
+  return cadena
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
